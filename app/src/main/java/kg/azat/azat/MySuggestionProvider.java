aae4c765384a909efb323a20068ef5e355f2e7a1
nurzamat <nurzamat@gmail.com>
2018-03-21 14:24:43 +0600

    first commit

--- app/src/main/java/kg/azat/azat/MySuggestionProvider.java
+++ app/src/main/java/kg/azat/azat/MySuggestionProvider.java
@@ -6,7 +6,7 @@ import android.content.SearchRecentSuggestionsProvider;
  * Created by nurzamat on 2/10/15.
  */
 public class MySuggestionProvider extends SearchRecentSuggestionsProvider {
-    public final static String AUTHORITY = "org.ananasit.azat.MySuggestionProvider";
+    public final static String AUTHORITY = "kg.azat.azat.MySuggestionProvider";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public MySuggestionProvider() {
